f6511b3e5c197244436334a7e72e0954063b1931
crscca2 <crscca2@yahoo.com>
2017-04-20 03:29:09 +0530

    witheureka

--- src/main/java/demo/AdminApplication.java
+++ src/main/java/demo/AdminApplication.java
@@ -5,17 +5,24 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableRedisHttpSession
+@EnableDiscoveryClient
+
 public class AdminApplication {
 
 	@RequestMapping("/user")
